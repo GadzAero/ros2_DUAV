7b5a5c5b3363da7317f238e0b8a725201ad48a96
St3p159789 <binon.stepane@gmail.com>
2025-01-02 15:33:49 +0100

    Avec premier code de MAV_pub depuis mon dualboot

---
+++ src/duav/duav/pub_MAV.py
@@ -0,0 +1,80 @@
+import rclpy
+from rclpy.node import Node
+from pymavlink import mavutil
+from std_msgs.msg import String
+from std_msgs.msg import Float32
+
+
+class MAVPublisher(Node):
+    def __init__(self):
+        super().__init__('pub_MAV')
+
+        # Connexion à MAVLink
+        self.connection = mavutil.mavlink_connection('tcp:127.0.0.1:5762')  # Ajustez si nécessaire
+        self.get_logger().info("En attente du heartbeat MAVLink...")
+        self.connection.wait_heartbeat()
+        self.get_logger().info("Connexion MAVLink établie !")
+
+
+        self.publisher_1 = self.create_publisher(String, 'IN/MAV/gps_coord', 10)
+        self.timer_1 = self.create_timer(1.0, self.timer_callback)  # 1 second interval
+        self.get_logger().info("Publisher Node Started")
+        
+        self.publisher_2 = self.create_publisher(String, 'IN/MAV/fences', 10)
+        self.timer_2 = self.create_timer(1.0, self.timer_callback)  # 1 second interval
+        self.get_logger().info("Publisher Node Started")
+        
+        self.publisher_3 = self.create_publisher(String, 'IN/MAV/altitude', 10)
+        self.timer_3 = self.create_timer(1.0, self.timer_callback)  # 1 second interval
+        self.get_logger().info("Publisher Node Started")
+        
+        self.publisher_4 = self.create_publisher(String, 'IN/MAV/no_go_zones', 10)
+        self.timer_4 = self.create_timer(1.0, self.timer_callback)  # 1 second interval
+        self.get_logger().info("Publisher Node Started")
+        
+        self.publisher_5 = self.create_publisher(String, 'IN/MAV/imu', 10)
+        self.timer_5 = self.create_timer(1.0, self.timer_callback)  # 1 second interval
+        self.get_logger().info("Publisher Node Started")
+        
+        self.publisher_6 = self.create_publisher(String, 'IN/MAV/attitude', 10)
+        self.timer_6 = self.create_timer(1.0, self.timer_callback)  # 1 second interval
+        self.get_logger().info("Publisher Node Started")
+
+        self.publisher_battery = self.create_publisher(Float32, 'IN/MAV/battery', 10)
+        self.timer_7 = self.create_timer(1.0, self.timer_callback_battery)  # 1 second interval
+        self.get_logger().info("Pub Node Started : BATTERY")
+
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "MAV Status: Active"
+        self.publisher_1.publish(msg)
+        self.get_logger().info(f"Publishing: '{msg.data}'")
+
+
+    def timer_callback_battery(self):
+        try:
+            msg = self.connection.recv_match(type='BATTERY_STATUS', blocking=True)
+            if msg:
+                voltage_msg = Float32()
+                voltage_msg.data = msg.voltages[0] / 1000.0  # En volts
+                self.publisher_battery.publish(voltage_msg)
+                self.get_logger().info("OK --> Battery")
+            else:
+                self.get_logger().warning("Aucun message BATTERY_STATUS reçu.")
+        except Exception as e:
+            self.get_logger().error(f"Erreur lors de la récupération des données MAVLink: {e}")
+
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MAVPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/duav/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'duav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='step',
+    maintainer_email='binon.stepane@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'write_MAV = duav.write_MAV:main',
+            'pub_MAV = duav.pub_MAV:main',
+            'create_path = duav.create_path:main',
+            'analyse_CAM = duav.analyse_CAM:main',
+            'determine_plane_state = duav.determine_plane_state:main',
+            'select_mission_type = duav.select_mission_type:main',
+            'create_costmap = duav.create_costmap:main',
+        ],
+    },
+)
