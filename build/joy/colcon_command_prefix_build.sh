723c79dd2ef6ab28c98c5f3b4e8fdd4adb1a3ad9
St3p159789 <binon.stepane@gmail.com>
2025-01-03 17:38:49 +0100

    cleanning 2

--- build/joy/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/step/ros2_DUAV/install/sdl2_vendor/share/sdl2_vendor/package.sh"
--- build/ps4_controllers/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/step/ros2_DUAV/install/ps4_controllers'
