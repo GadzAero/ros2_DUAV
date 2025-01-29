9341d6d30665714d828f2f6c51b1322082c8fb65
St3p159789 <binon.stepane@gmail.com>
2025-01-29 23:33:27 +0100

    Sauvegarde avant passage sur rb4

---
+++ src/popeye/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'popeye'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='step',
+    maintainer_email='binon.stepane@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    entry_points={
+        'console_scripts': [
+            'MAV_manager = popeye.MAV_manager:main',
+        ],
+    },
+)
